14a5a90b6136e4b6f10895522cde4b36447918fb
Thomas Leichtfuss <thomaslfuss@gmx.de>
2013-04-16 21:48:52 +0200

    reworking daytime.py; no more use of struct_times

--- daytime.py
+++ daytime.py
@@ -18,87 +18,100 @@ class DayTime(datetime.time):
         total_seconds:  daytime in seconds as a float
 
     """
-
-    def __init__(self, *args, **kwargs):
-        """DayTime([hour[, minute[, second[, tzinfo]]]])
-        """
-        #TODO: __add__ doesn't support microseconds for now
-        super(DayTime, self).__init__(*args, **kwargs)
-        self._delta = datetime.timedelta(
-            hours = self.hour,
-            minutes = self.minute,
-            seconds = self.second,
-            )
-        self._total_seconds = self._delta.total_seconds()
-
     @classmethod
-    def from_struct_time(cls, struct_time):
+    def fromtime(cls, time):
         """
-        Build a DayTime from a struct_time.
+        Build a daytime from a datetime.time- or datetime.datetime-object.
         
         Args:
-            struct_time:    time.struct_time (see the reference of time-module)
-        
-        Returns a DayTime-object.
+            time:   datetime.time- or datetime.datetime-object
 
+        Returns a daytime.
         """
         return cls(
-            hour = struct_time.tm_hour,
-            minute = struct_time.tm_min,
-            second = struct_time.tm_sec
+            hour=time.hour,
+            minute=time.minute,
+            second=time.second,
+            microsecond=time.microsecond
             )
-
-    @classmethod
-    def from_seconds(cls, sec, local=True):
-        """
-        Build a DayTime from seconds.
-
-        Args:
-            sec:    integer or float. Interpreted as total amount of seconds
-                    since the epoch (see documentation of time-module).
-                    (Mind that to get the localized time in seconds you need to
-                    use time.time() - time.altzone.)
-            local:  boolean that specifies whether time.localtime or time.gmtime
-                    is used to construct the daytime.
-        
-        Returns a DayTime-object.
-
-        """
-        if local: struct_time = time.localtime(sec)
-        else: struct_time = time.gmtime(sec)
-        return cls.from_struct_time(struct_time)
 
     @classmethod
     def strptime(cls, string, format='%H:%M:%S'):
         """
-        Build a DayTime from a string and a format.
+        Build a daytime from a string and a format.
 
         Args:
             string:     string parsed according to the specified format
             format:     defaults to '%H:%M:%S'.
                         See the library reference manual for formatting codes.
         
-        Returns a DayTime-object.
+        Returns a daytime.
 
         """
-        return cls.from_struct_time(time.strptime(string, format))
+        return cls.fromtime(datetime.datetime.strptime(string, format))
+
+    @classmethod
+    def fromtimestamp(cls, timestamp):
+        """
+        Build a local daytime from timestamp.
+
+        Args:
+            sec:    a POSIX timestamp, such as is returned by time.time()
+
+        Returns a daytime.
+
+        """
+        return cls.fromtime(datetime.datetime.fromtimestamp(timestamp))
+
+    @classmethod
+    def utcfromtimestamp(cls, timestamp):
+        """
+        Build a utc-daytime from timestamp.
+
+        Args:
+            sec:    a POSIX timestamp, such as is returned by time.time()
+
+        Returns a daytime.
+
+        """
+        return cls.fromtime(datetime.datetime.utcfromtimestamp(timestamp))
+
+    @classmethod
+    def daytime(cls):
+        """
+        Returns the actual daytime.
+        """
+        return cls.fromtime(datetime.datetime.today())
 
     @property
-    def delta(self):
-        return self.delta
+    def as_timedelta(self):
+        """
+        Daytime as timedelta.
+        """
+        return datetime.timedelta(
+            hours=self.hour,
+            minutes=self.minute,
+            seconds=self.second,
+            microseconds=self.microsecond
+            )
 
     @property
-    def total_seconds(self):
-        return self._total_seconds
+    def as_seconds(self):
+        """
+        Absolute amount of seconds of the daytime.
+        """
+        return self.as_timedelta.total_seconds()
 
     def __add__(self, other, sign=1):
-        # TODO: doing sums with datetime.timedelta-objects
         if isinstance(other, int) or isinstance(other, float):
-            seconds = self._total_seconds + sign * other
-            return DayTime.from_seconds(seconds, False)
+            seconds = self.as_seconds + sign * other
+            return DayTime.utcfromtimestamp(seconds)
         elif isinstance(other, DayTime):
-            seconds = self._total_seconds + sign * other._total_seconds
-            return DayTime.from_seconds(seconds, False)
+            seconds = self.as_seconds + sign * other.as_seconds
+            return DayTime.utcfromtimestamp(seconds)
+        elif isinstance(other, datetime.timedelta):
+            seconds = self.as_seconds + sign * other.total_seconds()
+            return DayTime.utcfromtimestamp(seconds)
         else: raise TypeError("unsupported operator for DayTime and {0}".format(
             other.__class__.__name__))
 
@@ -107,32 +120,32 @@ class DayTime(datetime.time):
 
     def __gt__(self, other):
         if isinstance(other, int) or isinstance(other, float):
-            return self._total_seconds > other
+            return self.as_seconds > other
         else: return super(DayTime, self).__gt__(other)
 
     def __ge__(self, other):
         if isinstance(other, int) or isinstance(other, float):
-            return self._total_seconds >= other
+            return self.as_seconds >= other
         else: return super(DayTime, self).__ge__(other)
 
     def __lt__(self, other):
         if isinstance(other, int) or isinstance(other, float):
-            return self._total_seconds < other
+            return self.as_seconds < other
         else: return super(DayTime, self).__lt__(other)
 
     def __le__(self, other):
         if isinstance(other, int) or isinstance(other, float):
-            return self._total_seconds <= other
+            return self.as_seconds <= other
         else: return super(DayTime, self).__le__(other)
 
     def __eq__(self, other):
         if isinstance(other, int) or isinstance(other, float):
-            return self._total_seconds == other
+            return self.as_seconds == other
         else: return super(DayTime, self).__eq__(other)
 
     def __ne__(self, other):
         if isinstance(other, int) or isinstance(other, float):
-            return self._total_seconds != other
+            return self.as_seconds != other
         else: return super(DayTime, self).__ne__(other)
 
 
--- tests.py
+++ tests.py
@@ -5,21 +5,21 @@ from daytime import DayTime
 
 class DayTimeTest(unittest.TestCase):
     def setUp(self):
-        self.daytime1 = DayTime(0, 0, 0)
-        self.daytime2 = DayTime(2, 20, 22)
-        self.daytime3 = DayTime(2, 20, 23)
-        self.daytime4 = DayTime(12, 25, 0)
-        self.daytime5 = DayTime(18, 43, 20)
+        self.daytime1 = DayTime(0, 0, 0, 8000)
+        self.daytime2 = DayTime(2, 20, 22, 7000)
+        self.daytime3 = DayTime(2, 20, 23, 90000)
+        self.daytime4 = DayTime(12, 25, 0, 8400)
+        self.daytime5 = DayTime(18, 43, 20, 6000)
 
     def test_comparison(self):
         self.assertTrue(self.daytime1 < self.daytime3 > self.daytime2)
         self.assertTrue(self.daytime4 <= self.daytime4 >= self.daytime3)
         self.assertEqual(self.daytime4, self.daytime4)
-        self.assertTrue(self.daytime3.total_seconds < self.daytime4 <= self.daytime5.total_seconds)
+        self.assertTrue(self.daytime3.as_seconds < self.daytime4 <= self.daytime5.as_seconds)
 
     def test_addition(self):
-        self.assertTrue(self.daytime2 + self.daytime3 == time(4,40,45))
-        self.assertTrue(self.daytime2 - self.daytime3 == time(23,59,59))
+        self.assertTrue(self.daytime2 + self.daytime3 == time(4,40,45,97000))
+        self.assertTrue(self.daytime2 - self.daytime3 == time(23,59,58,917000))
 
 
 if __name__ == '__main__':
