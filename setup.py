79ce17d86d67ff3c0c10edfd8c6b29fc250ae406
Thomas Leichtfuss <thomaslfuss@gmx.de>
2013-04-19 16:35:16 +0200

    fix download-url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = "Thomas Leichtfuss", 
     author_email = "thomaslfuss@gmx.de",
     url = "https://github.com/thomst/daytime",
-    download_url = "https://pypi.python.org/packages/source/t/daytime/daytime-{version}.tar.gz".format(version=VERSION),
+    download_url = "https://pypi.python.org/packages/source/d/daytime/daytime-{version}.tar.gz".format(version=VERSION),
     description = 'Extension for datetime.time with the main focus on comarison an making sums.',
     long_description = "This module extends the datetime.time-module and makes it more handy respectivly to comparison, addition and substraction. You can compare, add and substract a daytime with another daytime, a datetime.time-object or an int or float as total amount of seconds. Making sums is also possible with a datetime.timedelta.",
     py_modules = ["daytime"],
